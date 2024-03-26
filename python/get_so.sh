e2b313f13c11107aefcd5da079b91c5299f4e615
Pranay <66739544+PranayB003@users.noreply.github.com>
2024-03-26 19:02:32 +0000

    fixed: missing shared object files in lambda layer

---
+++ python/get_so.sh
@@ -0,0 +1,14 @@
+mkdir -p /opt/fmi/python/so_files
+
+cp /lib64/libcurl.so.4 /opt/fmi/python/so_files/
+cp /lib64/libnghttp2.so.14 /opt/fmi/python/so_files/
+cp /lib64/libidn2.so.0 /opt/fmi/python/so_files/
+cp /lib64/libssh2.so.1 /opt/fmi/python/so_files/
+cp /lib64/libpsl.so.5 /opt/fmi/python/so_files/
+cp /lib64/libldap-2.4.so.2 /opt/fmi/python/so_files/
+cp /lib64/liblber-2.4.so.2 /opt/fmi/python/so_files/
+cp /lib64/libunistring.so.0 /opt/fmi/python/so_files/
+cp /lib64/libsasl2.so.3 /opt/fmi/python/so_files/
+cp /lib64/libssl3.so /opt/fmi/python/so_files/
+cp /lib64/libsmime3.so /opt/fmi/python/so_files/
+cp /lib64/libnss3.so /opt/fmi/python/so_files/
